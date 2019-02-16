641007962f49f5c0bc7ef137db005e9a90b0e037
Augusto César Melo <augustocesarmelo@gmail.com>
2019-02-16 16:40:52 -0300

    Endpoint /clientes/{i} disponível

---
+++ src/main/java/com/augustoakuma/cursomc/resources/ClienteResource.java
@@ -0,0 +1,36 @@
+package com.augustoakuma.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.augustoakuma.cursomc.domain.Cliente;
+import com.augustoakuma.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {	
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find( @PathVariable Integer id ) {		
+		Cliente obj = service.buscar(id);		
+		return ResponseEntity.ok().body(obj);	
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Cliente> listar() {		
+		List<Cliente> lista =	service.listarTodos();		
+		return lista;
+	}
+	
+}
