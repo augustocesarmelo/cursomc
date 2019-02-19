61e6ec6f5ca58d0a9fada6dd057acfe00937f0cf
Augusto César Melo <augustocesarmelo@gmail.com>
2019-02-18 22:41:35 -0300

    Pedido, Pagamento, EstadoPagamento

---
+++ src/main/java/com/augustoakuma/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.augustoakuma.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.augustoakuma.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
