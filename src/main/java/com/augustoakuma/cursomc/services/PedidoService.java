c7ae71b51135a95209220e5e017d4f8e1afb0b5f
Augusto César Melo <augustocesarmelo@gmail.com>
2019-02-24 15:53:36 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/augustoakuma/cursomc/services/PedidoService.java
@@ -0,0 +1,29 @@
+package com.augustoakuma.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.augustoakuma.cursomc.domain.Pedido;
+import com.augustoakuma.cursomc.repositories.PedidoRepository;
+import com.augustoakuma.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+		
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public List<Pedido> listarTodos() {
+		List<Pedido> obj = repo.findAll();
+		return obj;
+	}
+}
